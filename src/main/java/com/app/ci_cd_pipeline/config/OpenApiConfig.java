56c9f96fe59012803cc705525e7b370945b199e2
alhajcse <alhajcse09@gmail.com>
2025-05-15 18:14:26 +0600

    update swagger name

--- src/main/java/com/app/ci_cd_pipeline/config/OpenApiConfig.java
+++ src/main/java/com/app/ci_cd_pipeline/config/OpenApiConfig.java
@@ -47,7 +47,7 @@ public class OpenApiConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Bitcoin App API")
+                .title("Bitcoin App Sample API")
                 .description("API Bitcoin")
                 .version("1.0")
                 .contact(apiContact())
